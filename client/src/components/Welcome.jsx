9b666b4dd65175fb4232410bfacc03f54132897b
Risha <rishamkeerthi@gmail.com>
2024-10-10 14:18:35 +0530

    added functionality to connect to MetaMask

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { AiFillPlayCircle } from "react-icons/ai";
+import { useContext } from "react";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
-// import { TransactionContext } from "../context/TransactionContext";
-// import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
+import { TransactionContext } from "../context/TransactionContext";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -22,9 +20,8 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 
 const Welcome = () => {
 
-  const connectWallet = () => {
+  const { connectWallet, currentAccount } = useContext(TransactionContext);
 
-  }
   const handleSubmit = () => {
 
   }
@@ -40,7 +37,9 @@ const Welcome = () => {
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
-          <button type="button" onClick={connectWallet()} className="bg-[#2952e3] text-white rounded-full px-4 cursor-pointer my-4 flex hover:bg-[#2546bd] w-full text-center justify-center items-center py-2">Connect Wallet</button>
+          {!currentAccount && (
+          <button type="button" onClick={connectWallet} className="bg-[#2952e3] text-white rounded-full px-4 cursor-pointer my-4 flex hover:bg-[#2546bd] w-full text-center justify-center items-center py-2">Connect Wallet</button>
+          )}
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-20">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Realiability</div>
             <div className={`${commonStyles} md:rounded-none rounded-tr-2xl`}>Security</div>
